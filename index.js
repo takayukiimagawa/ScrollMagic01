fe7d360a472e41a3be2f862724a9d5245eda681f
Takayuki Imagawa <imapaso16@gmail.com>
2019-06-12 18:01:19 +0900

    Styled sec01 to sec03

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const tl = new TimelineMax();
+const tl02 = new TimelineMax();
 const controller = new ScrollMagic.Controller();
 
 tl.from("#hello-world", 0, { opacity: 0 });
 tl.from("#hello-world-cover", 0.3, { width: 0 });
 tl.to("#hello-world-cover", 1, { width: 0, x: 2000 });
 
-const scene = new ScrollMagic.Scene({
+tl02.from("#sec03-title", 0.3, { x: -2000 });
+tl02.to("#sec03-title", 0.3, { x: 100 });
+tl02.from(".img", 1, { height: 0 });
+
+const scene02 = new ScrollMagic.Scene({
   triggerElement: ".sec02",
-  triggerHook: "onLeave",
+  //offset: 150,
+  triggerHook: "onCenter",
   duration: "100%"
 })
   .setPin(".sec02")
   .setTween(tl)
+  .addTo(controller);
+
+const scene03 = new ScrollMagic.Scene({
+  triggerElement: ".sec03",
+  offset: -500,
+  duration: "100%"
+})
+  //.setPin(".sec03")
+  .setTween(tl02)
   .addTo(controller);
